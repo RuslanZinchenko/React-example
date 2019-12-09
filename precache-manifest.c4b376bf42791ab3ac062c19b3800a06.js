af62a3f93b9c3ab6ecf93e933e1413b8321ba7f0
RuslanZinchenko <zinchenko.rusya@gmail.com>
2019-12-09 22:27:31 +0200

    Updates

---
+++ precache-manifest.c4b376bf42791ab3ac062c19b3800a06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950cf23afb04dbdca5c6750228ba726d",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "acd7774abbdf80ba4a96",
+    "url": "/react-example/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa5e86eab4a57ff7004d",
+    "url": "/react-example/static/js/2.8bc86a04.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-example/static/js/2.8bc86a04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acd7774abbdf80ba4a96",
+    "url": "/react-example/static/js/main.2708ee87.chunk.js"
+  },
+  {
+    "revision": "0c1b8f0834f2a98c77bb",
+    "url": "/react-example/static/js/runtime-main.5549c72a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
